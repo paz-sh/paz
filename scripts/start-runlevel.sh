18d37c801fac6b02ffd1ff673d3005b33795c5d4
Luke Bond <luke.n.bond@gmail.com>
2015-02-16 03:17:30 +0000

    Whoops missing semi-colon.
    Fixes #10

--- scripts/start-runlevel.sh
+++ scripts/start-runlevel.sh
@@ -2,7 +2,7 @@
 [[ $1 ]] || { echo "Missing numeric runlevel argument (ie. 1 or 2)"; exit 1; }
 [[ $ETCD_ENDPOINT ]] || ETCD_ENDPOINT=172.17.8.101:4001
 
-etcdctl --peers=$ETCD_ENDPOINT ls > /dev/null 2>&1 || (echo "etcd unreachable at $ETCD_ENDPOINT" exit 1)
+etcdctl --peers=$ETCD_ENDPOINT ls > /dev/null 2>&1 || (echo "etcd unreachable at $ETCD_ENDPOINT"; exit 1)
 
 echo Starting paz runlevel $1 units
 fleetctl -strict-host-key-checking=false start unitfiles/$1/*
